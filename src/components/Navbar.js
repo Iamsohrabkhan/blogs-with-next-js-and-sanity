b725a09ce123684d29c0d7f63e24c519100247a0
Sohrab Khan <7ohrabkhan@gmail.com>
2023-07-29 23:51:28 -0700

    adding <hr/> in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,51 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 import Themebutton from "./themebutton";
 const Navbar = () => {
   const active = usePathname();
- 
-  return (
-  
-    <header className="dark:bg-[rgba(31,34,41,0.83)]  dark:shadow-sm backdrop-blur-sm fixed top-0 w-full z-50 bg-white/80 ">
 
-  
-  <nav className="w-full max-w-3xl mx-auto py-4 ">
-    <div className="flex justify-between items-center px-16 md:px-10 lg:px-0">
-      <div className="item ">
-        <ul className="flex items-center gap-5">
-          <li><Link href="/" className={` text-base font-serif  ${active==="/"? "text-indigo-500" : "dark:text-white"}`}>Home</Link></li>
-          <li><Link href={"/blogs" } className={` text-base font-serif  ${active==="/blogs" ? "text-indigo-500" : "dark:text-white"}`}>Blogs</Link></li>
-        </ul>
-      </div>
-      <div className="mode">
-      <Themebutton/>
-      </div>      
-    </div>
-   
-  </nav>
-  </header>
+  return (
+    <>
+      <header className="dark:bg-dark/80  shadow-sm backdrop-blur-sm fixed top-0 w-full z-50 bg-white/80 ">
+        <nav className="w-full max-w-3xl mx-auto py-4 ">
+          <div className="flex justify-between items-center px-16 md:px-10 lg:px-0">
+            <div className="item ">
+              <ul className="flex items-center gap-5">
+                <li>
+                  <Link
+                    href="/"
+                    className={` text-base font-serif  ${
+                      active === "/" ? "text-indigo-500" : "dark:text-white"
+                    }`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/blogs"}
+                    className={` text-base font-serif  ${
+                      active === "/blogs"
+                        ? "text-indigo-500"
+                        : "dark:text-white"
+                    }`}
+                  >
+                    Blogs
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mode">
+              <Themebutton />
+            </div>
+          </div>
+        </nav>
+        <div className="container max-w-md sm:max-w-xl md:max-w-2xl customb:max-w-4xl lg:max-w-6xl mx-auto ">
+          <hr />
+        </div>
+      </header>
+    </>
   );
 };
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: "#20232A",
+        dark: "#000000",
         light: "#fff",
         light_primary: "#eeeeee",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        caveat: ['Caveat', 'cursive'],
+        inter: ["Inter", "sans-serif"],
+        caveat: ["Caveat", "cursive"],
       },
       animation: {
         "spin-slow": "spin 0.2s linear 1",
+      },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        customb : "950px",
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
     },
   },
